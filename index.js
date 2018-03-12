7103e6666e05ea5149d85b6587161a9779c5ced8
bagisoft <37068873+bagisoft@users.noreply.github.com>
2018-03-12 15:37:45 +0700

    Update index.js

--- index.js
+++ index.js
@@ -23,9 +23,7 @@ var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
 if(x=="1"){xxx="7-useful-tips-for-optimizing.html"}
 if(x=="2"){xxx="basic-knowledge-of-auctions-for.html"}
-if(x=="3"){xxx="year-end-is-perfect-time-to-review-your.html"}
-if(x=="4"){xxx="some-tips-for-designing-effective.html"}
-if(x=="5"){xxx="6-perfect-ideas-for-corporate-event.html"}a_to_ve[i].href="https://www.bagisoft.net/2018/03/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="3"){xxx="year-end-is-perfect-time-to-review-your.html"}a_to_ve[i].href="https://www.bagisoft.net/2018/03/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
